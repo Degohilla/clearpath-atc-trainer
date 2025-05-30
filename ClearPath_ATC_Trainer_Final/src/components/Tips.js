9feb1a081e0566262dc52ac19a3fb5c049bdc2d5
Degohilla <Mrteejack@gmail.com>
2025-05-30 12:58:26 +0100

    Add files via upload

---
+++ ClearPath_ATC_Trainer_Final/src/components/Tips.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+export default function Tips() {
+  return (
+    <div>
+      <h2>Beginner Tips</h2>
+      <p>Useful tips for ATC communication.</p>
+    </div>
+  );
+}
